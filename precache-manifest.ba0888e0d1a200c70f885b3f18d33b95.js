5c12a4f03bb3cbcfc2fda45e0fcaea9f01db574c
mrmauricio <mendes-mauricio@outlook.com~>
2019-07-16 15:13:52 +0100

    Updates

---
+++ precache-manifest.ba0888e0d1a200c70f885b3f18d33b95.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4fd74347c130b9be1468b8ba82dda1",
+    "url": "/repo-manager/index.html"
+  },
+  {
+    "revision": "da095f3eec188c5d9798",
+    "url": "/repo-manager/static/js/2.d4df579c.chunk.js"
+  },
+  {
+    "revision": "e8d43fced82055a5e94d",
+    "url": "/repo-manager/static/js/main.169a4510.chunk.js"
+  },
+  {
+    "revision": "da0d1fa4886bf97691fb",
+    "url": "/repo-manager/static/js/runtime~main.3e78509e.js"
+  }
+]);
